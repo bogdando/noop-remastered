5f1c04a751af41fac7d4bae7ff99165614d05556
Bogdan Dobrelya <bogdando@mail.ru>
2021-05-04 10:30:11 +0200

    Add hiera config to pick it automagically
    Signed-off-by: Bogdan Dobrelya <bogdando@mail.ru>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,7 @@ RSpec.configure do |c|
   c.mock_with :rspec
   c.module_path = File.join(fixture_path, 'modules')
   c.manifest_dir = File.join(fixture_path, 'manifests')
+  c.hiera_config = '/etc/puppet/hiera.yaml'
 end
 
 at_exit { RSpec::Puppet::Coverage.report! }
